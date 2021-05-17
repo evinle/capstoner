d4a5199eb17e0bb7dd3a67d288052a24e0ea7d61
evinle <imevinle@gmail.com>
2021-05-17 21:33:04 +0800

    Added comments for Map, Home and App

--- src/Home.js
+++ src/Home.js
@@ -9,38 +9,81 @@ function Home() {
   // State management for view state of the map
   const [viewState, setViewState] = React.useState({
     ...Locations.Perth,
-    zoom: 5,
+    zoom: 9,
     pitch: 40,
     bearing: 0,
   });
 
-  const interp = new FlyToInterpolator();
+  const interp = new FlyToInterpolator({ speed: 0.85 });
 
+  // area/hexagon the user clicks on the map
   const [chosenArea, setChosenArea] = React.useState({});
+
+  // this state determines if we're showing the general stats for an area or not
   const [showAreaStats, setShowAreaStats] = React.useState(false);
 
+  // when the user wants to move, this function will be called to update the
+  // viewstate based on their input
   const handleViewStateChange = ({ viewState }) => {
     setViewState({
       ...viewState,
     });
   };
 
+  //  const determineZoom = () => {
+  //     if (!chosenArea.demand || !chosenArea.supply) return 12;
+
+  //     console.log(
+  //       "determined zoom: ",
+  //       14 -
+  //         Math.max(
+  //           Object.keys(chosenArea.demand.food).reduce(
+  //             (total, current) => total + chosenArea.demand.food[current],
+  //             0
+  //           ),
+  //           Object.keys(chosenArea.supply.food).reduce(
+  //             (total, current) => total + chosenArea.demand.food[current],
+  //             0
+  //           )
+  //         ) /
+  //           9000
+  //     );
+  //     return (
+  //       14 -
+  //       Math.max(
+  //         Object.keys(chosenArea.demand.food).reduce(
+  //           (total, current) => total + chosenArea.demand.food[current],
+  //           0
+  //         ),
+  //         Object.keys(chosenArea.supply.food).reduce(
+  //           (total, current) => total + chosenArea.demand.food[current],
+  //           0
+  //         )
+  //       ) /
+  //         9000
+  //     );
+  //   };
+
+  // fancy fly-to logic to enable our map to fly to a town the user's clicked
   const handleAreaClick = (area) => {
     setShowAreaStats(true);
     setChosenArea(area);
     setViewState({
-      longitude: parseFloat(area.lng),
+      longitude: parseFloat(area._long),
       latitude: parseFloat(area.lat),
-      zoom: 9,
+      zoom: 13,
       pitch: 40,
-      bearing: 0,
-      transitionDuration: 7500 * (1 / viewState.zoom),
+      bearing: viewState.bearing,
+      // we want the transition duration to be inversely proportional to the
+      // zoom i.e if the user's really zoomed in, we want that time to be smaller
+      transitionDuration: 10000 / viewState.zoom,
       transitionInterpolator: interp,
     });
   };
 
   return (
     <>
+      {/* we do this instead of the && syntax to enable transitions  */}
       <ul
         className={
           showAreaStats ? "general-stats-tab" : "general-stats-tab closed"
@@ -53,10 +96,22 @@ function Home() {
         />
 
         {Object.keys(chosenArea).map((stat) => {
+          if (stat !== "supply" && stat !== "demand") {
+            return (
+              // logic to display all the key value pairs in the object
+              <li key={stat}>{`${stat.charAt(0).toUpperCase()}${stat.slice(
+                1
+              )} : ${chosenArea[stat]}`}</li>
+            );
+          }
           return (
-            <li>{`${stat.charAt(0).toUpperCase()}${stat.slice(1)} : ${
-              chosenArea[stat]
-            }`}</li>
+            <li key={stat}>{`${stat.charAt(0).toUpperCase()}${stat.slice(
+              1
+            )} : [Meat: ${chosenArea[stat].food.meat}\nCarbs: ${
+              chosenArea[stat].food.carbs
+            }\nVeg: ${chosenArea[stat].food.vegetables}\nFruits: ${
+              chosenArea[stat].food.fruits
+            } ]`}</li>
           );
         })}
       </ul>
@@ -65,7 +120,13 @@ function Home() {
         <Map
           width="100vw"
           height="100vh"
-          chosenArea={chosenArea}
+          //areaOnClick is the handler we pass in for when the user
+          // clicks on a town/hexagon. Since we want to be able to
+          // extract information from the clicked object and display it
+          // on the page on top of the map, we implement the logic here
+          // instead of inside the map component itself. We also don't
+          // want this to be a generic function of the map but rather
+          // a feature of this "Home" component
           areaOnClick={handleAreaClick}
           viewState={viewState}
           onViewStateChange={handleViewStateChange}
--- src/map/Map.js
+++ src/map/Map.js
@@ -2,11 +2,9 @@ import "./mapstyles.css";
 import React from "react";
 import ReactMap from "react-map-gl";
 import DeckGL from "@deck.gl/react";
-// import * as Locations from "./locations";
-// import { ScatterplotLayer, GeoJsonLayer } from "@deck.gl/layers";
 import { HexagonLayer } from "@deck.gl/aggregation-layers";
-// import Suburbs from "./subs.json";
-import AUPopulationAtLocation from "../au-cities-population-location.json";
+// import AUPopulationAtLocation from "../au-cities-population-location.json";
+import AUPopulationAtLocation from "../GeoFeature.json";
 
 const Map = ({
   height,
@@ -17,143 +15,6 @@ const Map = ({
   areaOnClick,
   style,
 }) => {
-  // const locNameArray = [...Object.keys(Locations)];
-  // let locArray = [];
-  // locNameArray.forEach((e) => {
-  //   locArray = [...locArray, Locations[e]];
-  // });
-
-  // // const locArrayConst = locArray;
-
-  // locArray = Suburbs.features;
-
-  // const suburbsArray = locArray.filter((entry) => {
-  //   return (
-  //     (entry.properties.wa_local_2 === "PERTH") |
-  //     (entry.properties.wa_local_2 === "ROCKINGHAM") |
-  //     (entry.properties.wa_local_2 === "JOONDALUP")
-  //   );
-  // });
-
-  // const maxPopulation = React.useRef(0);
-
-  // const ting = suburbsArray.forEach((entry) => {
-  //   fetch(
-  //     `https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&q=&facet=country&refine.country=au&refine.city=${entry.properties.wa_local_2.toLowerCase()}` //name of city
-  //   )
-  //     .then((rawData) => rawData.json())
-  //     .then((data) => {
-  //       return new Promise((resolve, reject) => {
-  //         if (data.nhits > 0) {
-  //           if (data.records[0].fields.population > maxPopulation.current) {
-  //             maxPopulation.current = data.records[0].fields.population;
-  //           }
-  //           resolve(data.records[0].fields.population);
-  //         } else {
-  //           resolve(0);
-  //         }
-  //       });
-  //     })
-  //     .then((data) => {
-  //       entry.properties.population = data;
-  //     })
-  //     .catch((err) => console.log(err));
-  // });
-
-  // React.memo(() => console.log(suburbsArray), [suburbsArray]);
-
-  // const geoJsonLaya = new GeoJsonLayer({
-  //   id: "geojson-layer",
-  //   data: { type: "FeatureCollection", features: [...suburbsArray] },
-  //   //data: RoadNetwork,
-  //   pickable: true,
-  //   stroked: true,
-  //   filled: true,
-  //   extruded: true,
-  //   lineWidthScale: 1,
-  //   lineWidthMinPixels: 2,
-  //   getFillColor: (d) => [
-  //     ((d.properties.population / maxPopulation.current) * 255) % 256,
-  //     0,
-  //     0,
-  //     100,
-  //   ],
-  //   getLineColor: (d) => [
-  //     ((d.properties.population / maxPopulation.current) * 255) % 256,
-  //     255,
-  //     255,
-  //     255,
-  //   ],
-  //   // getLineColor: [255, 91, 17],
-  //   // getFillColor: [255, 91, 17],
-  //   getRadius: 100,
-  //   getLineWidth: 2,
-  //   getElevation: 3,
-  //   onClick: (d) => console.log(d.object.properties.population),
-  // });
-
-  // const scatterplotLaya = new ScatterplotLayer({
-  //   id: "scatterplot-laya",
-  //   data: locArrayConst,
-  //   stroked: true,
-  //   filled: true,
-  //   getRadius: (d) => d.population,
-  //   radiusMaxPixels: 25,
-  //   radiusMinPixels: 5,
-  //   getPosition: (d) => [d.longitude, d.latitude],
-  //   getFillColor: (d) => [(d.population * 2) % 256, 110, 0, 175],
-  // });
-
-  // const layers = [
-  //   new HexagonLayer({
-  //     id: "hexagon-layer",
-  //     data: citiesWa.current,
-  //     pickable: true,
-  //     extruded: true,
-  //     radius: 20000,
-  //     getRadius: 10,
-  //     elevationScale: 500,
-  //     getPosition: (d) => [d.fields.longitude, d.fields.latitude],
-  //     getElevationWeight: (d) => d.fields.population,
-  //     getColorWeight: (d) => d.fields.population,
-  //     onClick: ({ object }) => {
-  //       if (object.points[0].source) {
-  //         console.log(object.points[0].source);
-  //       }
-  //     },
-  //     onHover: (d) => {
-  //       if (d.object) {
-  //         tooltip.current.style.opacity = 0.9;
-  //         tooltip.current.style.left = `${d.x}px`;
-  //         tooltip.current.style.top = `${d.y}px`;
-  //         tooltip.current.innerHTML = `<h2>${d.object.points[0].source.fields.city.toUpperCase()}</h2>
-  //       <p>Population: ${d.object.points[0].source.fields.population}</p>`;
-  //       } else {
-  //         tooltip.current.style.opacity = 0.0;
-  //         tooltip.current.style.left = `0px`;
-  //         tooltip.current.style.top = `0px`;
-  //       }
-  //     },
-  //   }),
-  // geoJsonLaya,
-  // ];
-
-  // const citiesWa = React.useRef({});
-
-  // React.useEffect(() => {
-  //   fetch(
-  //     "https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&q=&rows=20&sort=population&facet=country&refine.country=au&refine.region=08"
-  //   )
-  //     .then((data) => data.json())
-  //     .then((data) => {
-  //       console.log("Data: " + data);
-  //       citiesWa.current = data.records.filter((city) => {
-  //         return typeof city.fields.population !== "undefined";
-  //       });
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, []);
-
   const tooltip = React.useRef();
 
   const hexagonLayerData = AUPopulationAtLocation;
@@ -166,34 +27,91 @@ const Map = ({
     specularColor: [100, 30, 30],
   };
 
-  const populationHexagonLayer = new HexagonLayer({
-    id: "hexagon-layer",
+  const demandColorRange = [
+    // [25, 176, 6, 100],
+    // [255, 203, 1, 100],
+    // [205, 51, 1, 100],
+    [255, 0, 0, 100],
+  ];
+
+  const meatSupplyHexagonLayer = new HexagonLayer({
+    id: "meat-supply-hexagon-layer",
     data: hexagonLayerData,
-    pickable: true,
+    elevationScale: 8,
+    pickable: false,
+    radius: 200,
     extruded: true,
-    radius: 2000,
-    getRadius: 10,
-    elevationScale: 500,
-    getPosition: (d) => [parseFloat(d.lng), parseFloat(d.lat)],
-    getElevationWeight: (d) => {
-      return parseInt(d.population);
+    getPosition: (d) => [parseFloat(d._long), parseFloat(d.lat)],
+    getElevationValue: (d) => d[0].supply.food.meat,
+    colorRange: [[200, 150, 150]],
+    // colorRange: [[255, 51, 1, 100]],
+  });
+
+  const carbsSupplyHexagonLayer = new HexagonLayer({
+    id: "carbs-supply-hexagon-layer",
+    data: hexagonLayerData,
+    elevationScale: 8,
+    pickable: false,
+    radius: 200,
+    extruded: true,
+    getPosition: (d) => [parseFloat(d._long), parseFloat(d.lat)],
+    getElevationValue: (d) => d[0].supply.food.meat + d[0].supply.food.carbs,
+    colorRange: [[150, 115, 92]],
+  });
+
+  const vegSupplyHexagonLayer = new HexagonLayer({
+    id: "veg-supply-hexagon-layer",
+    data: hexagonLayerData,
+    elevationScale: 8,
+    pickable: false,
+    radius: 200,
+    extruded: true,
+    getPosition: (d) => [parseFloat(d._long), parseFloat(d.lat)],
+    getElevationValue: (d) =>
+      d[0].supply.food.meat +
+      d[0].supply.food.carbs +
+      d[0].supply.food.vegetables,
+    colorRange: [[133, 203, 51]],
+  });
+
+  const fruitsSupplyHexagonLayer = new HexagonLayer({
+    id: "fruits-supply-hexagon-layer",
+    data: hexagonLayerData,
+    elevationScale: 8,
+    pickable: true,
+    radius: 200,
+    extruded: true,
+    getPosition: (d) => [parseFloat(d._long), parseFloat(d.lat)],
+    getElevationValue: (d) => {
+      return (
+        d[0].supply.food.meat +
+        d[0].supply.food.carbs +
+        d[0].supply.food.vegetables +
+        d[0].supply.food.fruits
+      );
     },
-    getColorWeight: (d) => parseInt(d.population),
+    colorRange: [[220, 122, 30]],
     onClick: (d) => {
       if (areaOnClick) {
         if (d.object.points[0].source) {
           areaOnClick(d.object.points[0].source);
+          tooltip.current.style.opacity = 0.0;
+          tooltip.current.style.left = `-200px`;
+          tooltip.current.style.top = `-200px`;
         }
       }
     },
+    // as you can already extrapolate: Da-hover-event
     onHover: (d) => {
+      // whenever the user hovers over a hexagon, tool tip shows up with
+      // if and only if the object exists
       if (d.object) {
         tooltip.current.style.opacity = 0.9;
-        tooltip.current.style.left = `${d.x}px`;
-        tooltip.current.style.top = `${d.y}px`;
-        tooltip.current.innerHTML = `<h2>${d.object.points[0].source.city.toUpperCase()}</h2>
+        tooltip.current.innerHTML = `<h2>${d.object.points[0].source.suburb.toUpperCase()}</h2>
           <p>Population: ${d.object.points[0].source.population}<br></p>
           `;
+        tooltip.current.style.left = `${d.x}px`;
+        tooltip.current.style.top = `${d.y}px`;
       } else {
         tooltip.current.style.opacity = 0.0;
         tooltip.current.style.left = `-200px`;
@@ -201,6 +119,11 @@ const Map = ({
       }
     },
     onDrag: () => {
+      // There was a "feature" where when you hover a town and
+      // then starts to drag the map, the tooltip would still be
+      // showing but its location on the screen doesn't match the
+      // chosen location anymore, so i just choose to disable it
+      // if the user starts dragging
       if (tooltip.current.style.opacity === 0.0) {
         return;
       }
@@ -208,10 +131,153 @@ const Map = ({
       tooltip.current.style.left = `-200px`;
       tooltip.current.style.top = `-200px`;
     },
-    material: mats,
   });
 
-  const layers = [populationHexagonLayer];
+  const supplyHexagonLayer = new HexagonLayer({
+    id: "supply-hexagon-layer",
+    data: hexagonLayerData,
+    pickable: true,
+    extruded: true,
+    radius: 401,
+    // getRadius: 10,
+    elevationScale: 8,
+    // d is just the data in this case
+    getPosition: (d) => [parseFloat(d._long), parseFloat(d.lat)],
+    // getElevationWeight:
+    getColorWeight: (d) => {
+      return Object.keys(d.supply.food).reduce(
+        (total, current) => total + d.supply.food[current],
+        0
+      );
+    },
+    // d also stands for Da-click-event here
+    onClick: (d) => {
+      if (areaOnClick) {
+        if (d.object.points[0].source) {
+          areaOnClick(d.object.points[0].source);
+          tooltip.current.style.opacity = 0.0;
+          tooltip.current.style.left = `-200px`;
+          tooltip.current.style.top = `-200px`;
+        }
+      }
+    },
+    // as you can already extrapolate: Da-hover-event
+    onHover: (d) => {
+      // whenever the user hovers over a hexagon, tool tip shows up with
+      // if and only if the object exists
+      if (d.object) {
+        tooltip.current.style.opacity = 0.9;
+        tooltip.current.innerHTML = `<h2>${d.object.points[0].source.suburb.toUpperCase()}</h2>
+          <p>Population: ${d.object.points[0].source.population}<br></p>
+          `;
+        tooltip.current.style.left = `${d.x}px`;
+        tooltip.current.style.top = `${d.y}px`;
+      } else {
+        tooltip.current.style.opacity = 0.0;
+        tooltip.current.style.left = `-200px`;
+        tooltip.current.style.top = `-200px`;
+      }
+    },
+    onDrag: () => {
+      // There was a "feature" where when you hover a town and
+      // then starts to drag the map, the tooltip would still be
+      // showing but its location on the screen doesn't match the
+      // chosen location anymore, so i just choose to disable it
+      // if the user starts dragging
+      if (tooltip.current.style.opacity === 0.0) {
+        return;
+      }
+      tooltip.current.style.opacity = 0.0;
+      tooltip.current.style.left = `-200px`;
+      tooltip.current.style.top = `-200px`;
+    },
+    getElevationValue: (d) =>
+      d[0].supply.food.meat +
+      d[0].supply.food.carbs +
+      d[0].supply.food.vegetables +
+      d[0].supply.food.fruits,
+    material: mats,
+    colorRange: [[0, 255, 0, 100]],
+  });
+
+  const demandHexagonLayer = new HexagonLayer({
+    id: "demand-hexagon-layer",
+    data: hexagonLayerData,
+    pickable: true,
+    extruded: true,
+    radius: 401,
+    // getRadius: 10,
+    elevationScale: 8,
+    // d is just the data in this case
+    getPosition: (d) => [parseFloat(d._long), parseFloat(d.lat)],
+    // getElevationWeight:
+    getColorWeight: (d) =>
+      (d.demand.food.meat +
+        d.demand.food.carbs +
+        d.demand.food.vegetables +
+        d.demand.food.fruits) /
+      (d.supply.food.meat +
+        d.supply.food.carbs +
+        d.supply.food.vegetables +
+        d.supply.food.fruits),
+    // d also stands for Da-click-event here
+    onClick: (d) => {
+      if (areaOnClick) {
+        if (d.object.points[0].source) {
+          areaOnClick(d.object.points[0].source);
+          tooltip.current.style.opacity = 0.0;
+          tooltip.current.style.left = `-200px`;
+          tooltip.current.style.top = `-200px`;
+        }
+      }
+    },
+    // as you can already extrapolate: Da-hover-event
+    onHover: (d) => {
+      // whenever the user hovers over a hexagon, tool tip shows up with
+      // if and only if the object exists
+      if (d.object) {
+        tooltip.current.style.opacity = 0.9;
+        tooltip.current.innerHTML = `<h2>${d.object.points[0].source.suburb.toUpperCase()}</h2>
+          <p>Population: ${d.object.points[0].source.population}<br></p>
+          `;
+        tooltip.current.style.left = `${d.x}px`;
+        tooltip.current.style.top = `${d.y}px`;
+      } else {
+        tooltip.current.style.opacity = 0.0;
+        tooltip.current.style.left = `-200px`;
+        tooltip.current.style.top = `-200px`;
+      }
+    },
+    onDrag: () => {
+      // There was a "feature" where when you hover a town and
+      // then starts to drag the map, the tooltip would still be
+      // showing but its location on the screen doesn't match the
+      // chosen location anymore, so i just choose to disable it
+      // if the user starts dragging
+      if (tooltip.current.style.opacity === 0.0) {
+        return;
+      }
+      tooltip.current.style.opacity = 0.0;
+      tooltip.current.style.left = `-200px`;
+      tooltip.current.style.top = `-200px`;
+    },
+    getElevationValue: (d) =>
+      d[0].demand.food.meat +
+      d[0].demand.food.carbs +
+      d[0].demand.food.vegetables +
+      d[0].demand.food.fruits,
+    material: mats,
+    colorRange: demandColorRange,
+  });
+
+  const layers = [
+    // fruitsSupplyHexagonLayer,
+    // vegSupplyHexagonLayer,
+    // carbsSupplyHexagonLayer,
+    // meatSupplyHexagonLayer,
+    supplyHexagonLayer,
+    demandHexagonLayer,
+  ];
 
   return (
     <div className="map-container" onClick={onClick} style={style}>
